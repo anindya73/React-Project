20bf44bbdf306dbcfc97266e395bb5e902d136da
anindya73 <anindyakhanra90@gmail.com>
2026-07-10 12:22:29 +0530

    Changes in app.jsx

--- 04bgchanger/src/App.jsx
+++ 04bgchanger/src/App.jsx
@@ -1,31 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
 
 function App() {
-  const[ color, setColor ] = useState("olive");
-  function changeColor(color){
+  const [color, setColor] = useState("olive");
+  function changeColor(color) {
     setColor(color);
   }
   return (
-    <div className="w-full h-screen duration-200" style={{backgroundColor:color}}>
+    <div
+      className="w-full h-screen duration-200"
+      style={{ backgroundColor: color }}
+    >
       <div className="fixed flex flex-wrap justify-center bottom-12 w-full">
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-3 rounded-3xl">
-          <button 
-          onClick={()=>changeColor('red')}
-          className="outline-none px-4 py-1 rounded-full shadow-lg text-red-600">
+          <button
+            onClick={() => changeColor("red")}
+            className="outline-none px-4 py-1 rounded-full shadow-lg text-red-600"
+          >
             Red
           </button>
-          <button 
-          onClick={()=>changeColor('blue')}
-          className="outline-none px-4 py-1 rounded-full shadow-lg text-blue-600">
+          <button
+            onClick={() => changeColor("blue")}
+            className="outline-none px-4 py-1 rounded-full shadow-lg text-blue-600"
+          >
             Blue
           </button>
-          <button 
-          onClick={()=>changeColor('green')}
-          className="outline-none px-4 py-1 rounded-full shadow-lg text-green-600">
+          <button
+            onClick={() => changeColor("green")}
+            className="outline-none px-4 py-1 rounded-full shadow-lg text-green-600"
+          >
             Green
           </button>
         </div>
@@ -34,4 +40,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
